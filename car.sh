fe289a48418f0befb275370dd20c8b9a254d54d5
noah-roopull <nruppel@ggc.edu>
2024-10-07 16:31:28 -0400

    Update car.sh
    Removed "echo" fluff

--- car.sh
+++ car.sh
@@ -1,16 +1,11 @@
-echo "Working..." &&
 mkdir -v build build/res
 cp -v *.java build/ &&
 cp -vr res/* build/res/ &&
 cp -v Manifest.txt build/ &&
 cd build &&
-echo "Compiling..." &&
 javac -verbose *.java &&
-echo "Compressing..." &&
 jar cvfm Game.jar Manifest.txt * &&
 mv -v Game.jar ../Game.jar &&
 cd .. &&
-echo "Cleaning..." &&
 rm -rv build &&
-echo "Done. Running..."
 java -jar Game.jar
